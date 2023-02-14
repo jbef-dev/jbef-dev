005aaedb94a50c261e8f8438a5e258fa52e05427
Jorge Befan <jorge.befan@gmail.com>
2023-02-14 21:34:21 +0100

    testing re-importing

--- ui/Navbar/Navbar.tsx
+++ ui/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { MenuButton } from './MenuButton';
 import { NavMenu } from './NavMenu';
 import { LocalizedLink, useTranslations } from 'next-intl';
 import { Logo } from '@/components/Logo/Logo';
-import { NavBarContainer } from './NavBarContainer';
+import { NavBarContainer } from '@/ui/Navbar/NavBarContainer';
 import { NAVBAR_LINKS } from '@/config/constants/pageContent';
 
 export const Navbar = () => {
