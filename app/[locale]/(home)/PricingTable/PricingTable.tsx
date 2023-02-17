d45a8ba7f84333b45c8f2eaeb24e454f9e566b66
Jorge Befan <jorge.befan@gmail.com>
2023-02-17 23:41:59 +0100

    add row to pricing table

--- app/[locale]/(home)/PricingTable/PricingTable.tsx
+++ app/[locale]/(home)/PricingTable/PricingTable.tsx
@@ -36,9 +36,31 @@ export const PricingTable = () => {
           </div>
           <div className='flex flex-col items-center gap-y-4 justify-center grow'>
             <h4 className='text-4xl font-medium'>
-              100$<span className='text-xl'>/mo</span>
+              290$<span className='text-xl'>/mo</span>
             </h4>
             <Button flavor='outlined' buttonSize='lg' className='font-medium'>
+              Start now
+            </Button>
+          </div>
+        </li>
+
+        <li className='flex gap-x-2 py-10'>
+          <div className='flex flex-col gap-y-4 w-2/3'>
+            <h6 className='uppercase text-responsive-lg'>STANDARD</h6>
+            <ul className='grid grid-cols-2 w-full gap-2 justify-between font-light text-responsive-md'>
+              {[...new Array(8)].map((k, i) => (
+                <li key={i} className='flex items-center gap-x-2'>
+                  <FaCheck className='text-responsive-xs' />
+                  <span>Feature</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='flex flex-col items-center gap-y-4 justify-center grow'>
+            <h4 className='text-4xl font-medium'>
+              100$<span className='text-xl'>/mo</span>
+            </h4>
+            <Button flavor='gradient' buttonSize='lg' className='font-medium'>
               Start now
             </Button>
           </div>
