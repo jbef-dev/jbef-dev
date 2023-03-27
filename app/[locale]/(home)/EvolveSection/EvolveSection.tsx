ece6609859c1d6ac5f42d3966218f02e772513e4
Jorge Befan <jorge.befan@gmail.com>
2023-03-27 17:57:19 +0200

    include internationalized titles in evolve section

--- app/[locale]/(home)/EvolveSection/EvolveSection.tsx
+++ app/[locale]/(home)/EvolveSection/EvolveSection.tsx
@@ -2,16 +2,19 @@ import { Heading2 } from '@/ui/Typography';
 import { EvolveTitle } from './EvolveTitle';
 import { StandOutIcons } from './StandOutIcons';
 import { FlexContainer } from '@/ui/Containers';
+import { useTranslations } from 'next-intl';
 
 const EvolveSection = () => {
+  const t = useTranslations('pages.home.evolve');
+
   return (
     <FlexContainer flexCol center>
       <Heading2 className='flex items-center justify-center flex-col w-full max-w-screen-2xl'>
         <EvolveTitle className='self-start pl-[20%] font-medium' xStyle={0}>
-          EVOLVE
+          {t('titles.title1')}
         </EvolveTitle>
         <EvolveTitle className='font-extralight' xStyle={1}>
-          YOUR WEBSITE
+          {t('titles.title2')}
         </EvolveTitle>
       </Heading2>
 
@@ -19,10 +22,10 @@ const EvolveSection = () => {
 
       <Heading2 className='flex flex-col w-full max-w-screen-xl'>
         <EvolveTitle className='self-end pr-[15%] font-extralight' xStyle={2}>
-          STAND OUT
+          {t('titles.title3')}
         </EvolveTitle>
         <EvolveTitle className='self-center pl-10 font-medium' xStyle={3}>
-          ONLINE
+          {t('titles.title4')}
         </EvolveTitle>
       </Heading2>
     </FlexContainer>
