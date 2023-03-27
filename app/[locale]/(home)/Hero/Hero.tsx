615c50ebe985f22de97fdb0f4f59b54968d44b1a
Jorge Befan <jorge.befan@gmail.com>
2023-03-27 23:57:32 +0200

    fix CLS and make Hero a server component

--- app/[locale]/(home)/Hero/Hero.tsx
+++ app/[locale]/(home)/Hero/Hero.tsx
@@ -7,6 +7,7 @@ const Hero = () => {
   const t = useTranslations('pages.home.hero');
 
   return (
+    // use min-height to avoid CLS
     <div className='flex w-full min-h-[250lvh]'>
       <HeroTitles
         titles={[
@@ -17,23 +18,25 @@ const Hero = () => {
         ]}
       />
 
-      <div className='flex absolute max-md:left-[15vw] bottom-[18%] lg:right-[6vw] lg:bottom-[45%] font-sans text-responsive-xs gap-1 text-white mix-blend-difference'>
-        <div className='pt-1'>
-          <svg
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-            className='fill-white w-3 h-3'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z'
-            />
-          </svg>
+      <div className='absolute h-[100svh] top-0 max-w-screen-3xl flex w-full'>
+        <div className='flex absolute max-md:left-[15vw] bottom-[18%] lg:right-[6vw] lg:bottom-[45%] mix-blend-difference'>
+          <div className='mr-1'>
+            <svg
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              className='fill-white w-4 h-4'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z'
+              />
+            </svg>
+          </div>
+          <p className='text-responsive-xs font-sans text-white'>
+            Custom websites —<br /> down to earth service
+          </p>
         </div>
-        <p>
-          Custom websites —<br /> down to earth service
-        </p>
       </div>
     </div>
   );
