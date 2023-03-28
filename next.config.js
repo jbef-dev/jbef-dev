83a5a4405e32017d1c4d851f109a6529d9934889
Jorge Befan <jorge.befan@gmail.com>
2023-03-28 12:03:56 +0200

    update next config to cache the pages in the CDN

--- next.config.js
+++ next.config.js
@@ -17,4 +17,31 @@ const withNextIntl = require('next-intl/plugin')('./i18n/config.tsx');
 module.exports = withNextIntl({
   // i18nConfig: './i18n/config.tsx',
   ...nextConfig,
+
+  headers() {
+    return [
+      {
+        // Cache all content pages
+        source: '/((?!_next|assets|favicon.ico).*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: [
+              `s-maxage=` + 86400, // 1 day in seconds
+              `stale-while-revalidate=` + 31556952, // 1 Year in seconds
+            ].join(', '),
+          },
+        ],
+
+        // If you're deploying on a host that doesn't support the `vary` header (e.g. Vercel),
+        // make sure to disable caching for prefetch requests for Server Components.
+        missing: [
+          {
+            type: 'header',
+            key: 'Next-Router-Prefetch',
+          },
+        ],
+      },
+    ];
+  },
 });
