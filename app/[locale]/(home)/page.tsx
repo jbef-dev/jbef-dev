51170321890b4f22e8047d875d6f9dbef9c917f7
Jorge Befan <jorge.befan@gmail.com>
2023-04-06 23:59:30 +0200

    work on home page

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -27,8 +27,9 @@ export default async function Home({
 
   return (
     <PageContainer>
-      <SectionContainer flexCol pt={false} px={false} gap={false}>
+      <SectionContainer flexCol gap={false}>
         <NewHero
+          // title={dict['pages'].home.hero.title}
           titles={[
             dict['pages'].home.hero.titles.title1,
             dict['pages'].home.hero.titles.title2,
@@ -44,6 +45,36 @@ export default async function Home({
         {/*     dict['pages'].home.hero.titles.title4, */}
         {/*   ]} */}
         {/* /> */}
+      </SectionContainer>
+
+      <SectionContainer flexCol>
+        <div>
+          <svg
+            className='h-10 w-10'
+            viewBox='0 0 200 200'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M32 100.641C32 68.391 54.1651 41.3515 84 34.1102V1.28125C36.3772 8.98855 0 50.5392 0 100.641C0 150.742 36.3772 192.293 84 200V167.171C54.1651 159.93 32 132.89 32 100.641ZM200 100.641C200 150.742 163.623 192.293 116 200V167.171C145.835 159.93 168 132.89 168 100.641C168 68.391 145.835 41.3515 116 34.1102V1.28125C163.623 8.98855 200 50.5392 200 100.641Z'
+              fill='url(#paint0_linear_231_555)'
+            />
+            <defs>
+              <linearGradient
+                id='paint0_linear_231_555'
+                x1='157.5'
+                y1='33.0763'
+                x2='44.7421'
+                y2='148.561'
+                gradientUnits='userSpaceOnUse'
+              >
+                <stop offset='0.0509862' stopColor='#FFB6E1' />
+                <stop offset='1' stopColor='#FBE3EA' />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
 
         <SeparatorMargin />
       </SectionContainer>
