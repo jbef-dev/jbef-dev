a094fc90afd8576778a6a538db7e96da5db71d41
Jorge Befan <jorge.befan@gmail.com>
2023-04-11 12:48:39 +0200

    fix localeswitcher alignment and remove icon

--- ui/LocaleSwitcher/LocaleSwitcher.tsx
+++ ui/LocaleSwitcher/LocaleSwitcher.tsx
@@ -6,12 +6,10 @@ import { ComponentPropsWithoutRef, useRef, useState } from 'react';
 
 import useOutsideClick from '@/hooks/useOutsideClick';
 import { AnimatePresence, motion } from 'framer-motion';
-import { MdOutlineTranslate } from 'react-icons/md';
-import { IoTriangle } from 'react-icons/io5';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { customTransitions, customVariants } from '../animation';
-// import { getUnlocalizedPath } from '@/i18n/getUnlocalizedPath';
+import { FaChevronUp } from 'react-icons/fa';
 
 interface LocaleSwitcherProps extends ComponentPropsWithoutRef<'button'> {
   currentLocale: Locale;
@@ -40,49 +38,40 @@ export const LocaleSwitcher = ({
   useOutsideClick(toggleOpen, open, ref);
   const otherLocales = i18n.locales.filter(locale => locale != currentLocale);
 
-  // const dropdownVariants: Variants = {
-  //   initial: { opacity: 0, y: '-10%', scale: 0.95 },
-  //   animate: { opacity: 1, y: 0, scale: 1 },
-  //   exit: { opacity: 0, y: '-10%', scale: 0.95 },
-  // };
-
   return (
     <button
       className={clsx(
-        'group relative flex items-center py-1 justify-center font-title',
-        // open ? 'bg-black/50 backdrop-blur-lg' : '',
+        'group relative flex items-center justify-center py-1',
         className
       )}
       onClick={() => toggleOpen()}
       {...props}
     >
       <div
-        className='grid place-items-center leading-none grid-cols-3 gap-x-1 lg:gap-x-2'
+        className='grid grid-cols-2 place-items-center gap-x-1 leading-none lg:gap-x-2'
         ref={ref}
       >
-        <IoTriangle
+        <FaChevronUp
           className={clsx([
             'text-[10px] transition-transform duration-300',
             open ? 'rotate-0' : 'rotate-180',
           ])}
         />
-        {
-          <div className='overflow-hidden'>
-            <div className='relative flex flex-col transition duration-500 group-target:-translate-y-full group-hover:-translate-y-full group-active:-translate-y-full'>
-              <div>{currentLocale.toUpperCase()}</div>
-              <div className='absolute top-full'>
-                {currentLocale.toUpperCase()}
-              </div>
+
+        <div className='overflow-hidden'>
+          <div className='relative flex flex-col transition duration-500 group-target:-translate-y-full group-hover:-translate-y-full group-active:-translate-y-full'>
+            <div>{currentLocale.toUpperCase()}</div>
+            <div className='absolute top-full'>
+              {currentLocale.toUpperCase()}
             </div>
           </div>
-        }{' '}
-        <MdOutlineTranslate />
+        </div>
       </div>
       {children}
       <AnimatePresence mode='wait'>
         {open && (
           <motion.div
-            className='absolute top-full mt-1 py-2 gap-3 -z-10 flex bg-black/50 backdrop-blur-lg w-full flex-col overflow-hidden items-center justify-center'
+            className='absolute top-full -z-10 mt-1 grid w-full grid-cols-2 overflow-hidden bg-black py-1 backdrop-blur-lg'
             initial='initial'
             animate={open ? 'animate' : 'initial'}
             exit='initial'
@@ -90,19 +79,14 @@ export const LocaleSwitcher = ({
             variants={customVariants.appearMenu}
             transition={customTransitions.appearMenu}
           >
-            {otherLocales.map(locale => (
-              <Link
-                key={locale}
-                className='flex w-full items-center justify-center disabled:hidden'
-                // href={pathName || '/'}
-                href={redirectedPathName(locale)}
-                // href={getUnlocalizedPath(pathName)}
-                // locale={locale}
-              >
-                {locale.toUpperCase()}
-              </Link>
-            ))}
-            {/* </div> */}
+            <div></div>
+            <div className='flex flex-col items-end justify-center gap-3 px-0.5'>
+              {otherLocales.map(locale => (
+                <Link key={locale} href={redirectedPathName(locale)}>
+                  {locale.toUpperCase()}
+                </Link>
+              ))}
+            </div>
           </motion.div>
         )}
       </AnimatePresence>
