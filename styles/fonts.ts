2908aede4b370b0cdaad1e178dada2c33a53b262
Jorge Befan <jorge.befan@gmail.com>
2023-05-05 18:27:22 +0200

    update font later

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -7,17 +7,17 @@ export const fontSans = Inter({
   display: 'swap',
 });
 
-export const fontTitle = localFont({
-  // src: '../public/fonts/Nohemi-VF.ttf',
-  src: '../public/fonts/Meshed-VF.ttf',
-  variable: '--font-title',
-  display: 'swap',
-});
-
-// export const fontTitle = Poppins({
-//   subsets: ['latin'],
-//   weight: ['100', '200', '300', '400', '500', '600', '700'],
+// export const fontTitle = localFont({
+//   // src: '../public/fonts/Nohemi-VF.ttf',
+//   src: '../public/fonts/Meshed-VF.ttf',
 //   variable: '--font-title',
-//   // style: 'normal',
 //   display: 'swap',
 // });
+
+export const fontTitle = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+  variable: '--font-title',
+  // style: 'normal',
+  display: 'swap',
+});
