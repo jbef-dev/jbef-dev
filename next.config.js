5d5dddf6b32dd631e2ca28904cd6716ee4af8c47
Jorge Befan <jorge.befan@gmail.com>
2023-05-19 20:09:35 +0200

    test s-maxage=0 for CDN performance on Cloudflare

--- next.config.js
+++ next.config.js
@@ -10,33 +10,34 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       // Cache all content pages
-  //       source: '/((?!_next|assets|favicon.ico).*)',
-  //       headers: [
-  //         {
-  //           key: 'Cache-Control',
-  //           value: 's-maxage=86400, stale-while-revalidate=2592000',
-  //           // value: [
-  //           //   `s-maxage=` + 86400, // 1 day in seconds
-  //           //   `stale-while-revalidate=` + 31556952, // 1 Year in seconds
-  //           // ].join(', '),
-  //         },
-  //       ],
-  //
-  //       // If you're deploying on a host that doesn't support the `vary` header (e.g. Vercel),
-  //       // make sure to disable caching for prefetch requests for Server Components.
-  //       missing: [
-  //         {
-  //           type: 'header',
-  //           key: 'Next-Router-Prefetch',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        // Cache all content pages
+        source: '/((?!_next|assets|favicon.ico).*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            // value: 's-maxage=86400, stale-while-revalidate=2592000',
+            value: 's-maxage=0',
+            // value: [
+            //   `s-maxage=` + 86400, // 1 day in seconds
+            //   `stale-while-revalidate=` + 31556952, // 1 Year in seconds
+            // ].join(', '),
+          },
+        ],
+
+        // If you're deploying on a host that doesn't support the `vary` header (e.g. Vercel),
+        // make sure to disable caching for prefetch requests for Server Components.
+        missing: [
+          {
+            type: 'header',
+            key: 'Next-Router-Prefetch',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
